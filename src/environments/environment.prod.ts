5fae2a7ebbbb2b2410ae24abaf479607acac2791
Christopher Wright <wrightcr@umich.edu>
2022-04-25 17:35:38 -0400

    Social Media application

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+   apiKey: "AIzaSyAPd9ah_z6ZQMctw-q6Oq3t05aYv4Oi8Cg",
+   authDomain: "socialmediaapp-4047e.firebaseapp.com",
+   projectId: "socialmediaapp-4047e",
+   storageBucket: "socialmediaapp-4047e.appspot.com",
+   messagingSenderId: "815693631292",
+   appId: "1:815693631292:web:4f8fa61599ec518ea01839",
+   measurementId: "G-69TF6FL2GP"
+ }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ firebaseConfig: {
+  apiKey: "AIzaSyAPd9ah_z6ZQMctw-q6Oq3t05aYv4Oi8Cg",
+  authDomain: "socialmediaapp-4047e.firebaseapp.com",
+  projectId: "socialmediaapp-4047e",
+  storageBucket: "socialmediaapp-4047e.appspot.com",
+  messagingSenderId: "815693631292",
+  appId: "1:815693631292:web:4f8fa61599ec518ea01839",
+  measurementId: "G-69TF6FL2GP"
+}
+
 };
 
 /*
