0a2494797ecb78f8112faf52f335b81e0cf6086e
Christopher Wright <wrightcr@umich.edu>
2022-04-28 18:54:40 -0400

    Login method

--- src/app/tools/authenticator/authenticator.component.ts
+++ src/app/tools/authenticator/authenticator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseTSAuth } from 'firebasets/firebasetsAuth/FirebaseTSAuth';
 
 @Component({
   selector: 'app-authenticator',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthenticatorComponent implements OnInit {
   state = AuthenticatorCompState.LOGIN;
-  constructor() {}
+  firebasetsAuth: FirebaseTSAuth;
+  constructor() {
+    this.firebasetsAuth = new FirebaseTSAuth();
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+  }
+
+  onRegisterClick(
+    registerEmail: HTMLInputElement,
+    registerPassword: HTMLInputElement,
+    registerConfirmPassword: HTMLInputElement,
+  ){
+    let email = registerEmail.value;
+    let password = registerPassword.value;
+    let confirmPassword = registerConfirmPassword.value;
+
+    if(
+      this.isNotEmpty(email) &&
+      this.isNotEmpty(password) &&
+      this.isNotEmpty(confirmPassword) &&
+      this.isAMatch(password,confirmPassword)
+    )
+    {
+      this.firebasetsAuth.createAccountWith(
+        {
+          email: email,
+          password: password,
+          onComplete: (uc) => {
+            alert("Account created");
+            registerEmail.value = "";
+            registerPassword.value = "";
+            registerConfirmPassword.value = "";
+          },
+          onFail: (err) => {
+            alert("Falied to create account.");
+          }
+        }
+      );
+    }
+
+
+  }
+
+  isNotEmpty(text: string)
+  {
+    return text != null && text.length > 0;
+  }
+
+  isAMatch(text: string, comparedWith: string)
+  {
+    return text == comparedWith;
+  }
 
   onForgotPasswordClick() {
     this.state = AuthenticatorCompState.FORGOT_PASSWORD;
