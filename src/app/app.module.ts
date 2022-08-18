66d9e55603847026d483f564f01279363fde4055
Christopher Wright <wrightcr@umich.edu>
2022-08-18 00:24:00 -0400

    Post display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { PostFeedComponent } from './pages/post-feed/post-feed.component';
 import { CreatePostComponent } from './tools/create-post/create-post.component';
+import { PostComponent } from './tools/post/post.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { CreatePostComponent } from './tools/create-post/create-post.component';
     EmailVerificationComponent,
     ProfileComponent,
     PostFeedComponent,
-    CreatePostComponent
+    CreatePostComponent,
+    PostComponent
 
   ],
   imports: [
